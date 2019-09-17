6b999797717631e6e20f544dee9e366f1ebe3826
alila <908609588@qq.com>
2019-09-17 10:29:21 +0800

    add --no-sandbox

--- searchWeb.js
+++ searchWeb.js
@@ -7,8 +7,9 @@ module.exports = async function searchCNNV () {
 
   const browser = await puppeteer.launch({
     // headless: false,
-    executablePath: './chrome-mac/Chromium.app/Contents/MacOS/Chromium',
-    slowMo: 2500 // slow down by 250ms
+    // executablePath: './chrome-mac/Chromium.app/Contents/MacOS/Chromium',
+    args: ['--no-sandbox'],
+    slowMo: 2000 // slow down by 250ms
   })
 
   console.log(new Date() + ': browser launched!')
